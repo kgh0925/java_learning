2393ccf25e2888def089cb7d4dd6b4834b46304e
kim geunho <96340564+kgh0925@users.noreply.github.com>
2022-01-04 14:09:52 +0000

    I am learning Java, the first language I encounter.

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    String name = "black";
+    String job = "Student";
+
+    System.out.println(name + job);
+  }
+
+}
---
+++ study_file/day1 ~ day 7.java
@@ -0,0 +1,21 @@
+/* 1day
+public class Main {
+  public static void main(String[] args) {
+    int value = 30;
+    int result = value + 10;
+    
+    System.out.println(result);
+  }
+
+}
+
+public class Main {
+  public static void main(String[] args) {
+    String name = "black";
+    String job = "Student";
+
+    System.out.println(name + job);
+  }
+
+}
+*/
