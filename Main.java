3a61e9076a975dcd3c995789dce102380990a932
kim geunho <96340564+kgh0925@users.noreply.github.com>
2022-01-05 13:40:44 +0000

    I studied variables and input/output.

--- Main.java
+++ Main.java
@@ -1,9 +1,43 @@
+import java.util.Scanner;
+
+
 public class Main {
-  public static void main(String[] args) {
-    String name = "black";
-    String job = "Student";
+  public static void main (String[] args) throws Exception {
+    System.out.println("010-xxxx-xxxx\t[my name is KGH]\tArmy");
+    System.out.println("We are \"developers\"");
+    System.out.println("Spring\\Summer\\Fall\\Winter");
 
-    System.out.println(name + job);
-  }
+    boolean stop = true;
+    if(stop) {
+      System.out.println("Stop!");
 
+    }
+    else {
+      System.out.println("Start!");
+    }
+
+    System.out.printf("name_list [ 1 : %1$s, 2 : %2$s ]\n", "KGH", "KHW");
+
+    int keycode;
+    
+    while(true){
+      keycode = System.in.read();
+      System.out.println("Keycode = " + keycode);
+      if (keycode == 113){
+        break;
+      }
+    }
+    Scanner scanner = new Scanner(System.in);
+    String inputData;
+    
+    while (true) {
+      inputData = scanner.nextLine();
+      System.out.println("The entered text : \"" + inputData + "\"");
+      if (inputData.equals("q")) {
+        break;
+      }
+    }
+    
+
+  } 
 }
--- study_file/day1 ~ day 7.java
+++ study_file/day1 ~ day 7.java
@@ -1,4 +1,6 @@
-/* 1day
+/* 
+[1day]
+
 public class Main {
   public static void main(String[] args) {
     int value = 30;
@@ -17,5 +19,53 @@ public class Main {
     System.out.println(name + job);
   }
 
+}
+*/
+
+/*
+[2day]
+
+import java.util.Scanner;
+
+
+public class Main {
+  public static void main (String[] args) throws Exception {
+    System.out.println("010-xxxx-xxxx\t[my name is KGH]\tArmy");
+    System.out.println("We are \"developers\"");
+    System.out.println("Spring\\Summer\\Fall\\Winter");
+
+    boolean stop = true;
+    if(stop) {
+      System.out.println("Stop!");
+
+    }
+    else {
+      System.out.println("Start!");
+    }
+
+    System.out.printf("name_list [ 1 : %1$s, 2 : %2$s ]\n", "KGH", "KHW");
+
+    int keycode;
+    
+    while(true){
+      keycode = System.in.read();
+      System.out.println("Keycode = " + keycode);
+      if (keycode == 113){
+        break;
+      }
+    }
+    Scanner scanner = new Scanner(System.in);
+    String inputData;
+    
+    while (true) {
+      inputData = scanner.nextLine();
+      System.out.println("The entered text : \"" + inputData + "\"");
+      if (inputData.equals("q")) {
+        break;
+      }
+    }
+    
+
+  } 
 }
 */
