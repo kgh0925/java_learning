82c12e67ed8f0dc356205d00c60ba455b93d81c9
kim geunho <96340564+kgh0925@users.noreply.github.com>
2022-01-08 13:21:24 +0000

    Contents of the conditional sentence and the repeated sentence.

--- Main.java
+++ Main.java
@@ -1,29 +1,31 @@
 public class Main {
   public static void main (String [] args) {
-    int x = 5;
-    int y = 5;
-    int result1 = x++ + 5;
-    // 변수를 실행한 후 x에 1이 더해짐 [1 is added to x after executing the variable.]
-    int result2 = ++y + 5;
-    // y에 1이 더해지고 변수가 실행됨 [1 is added to y and the variable is executed.]
-    System.out.println("result1 = " + result1 + ", result2 = " + result2);
-    
-    //Using the Asky code.
+    int sum = 0;
 
-    char code = 'A';
+    for (int i = 1; i<= 100; ++i){
+      sum += i;
+    }
+    System.out.println("1~100 Total = " + sum );
 
-    if ((code >= 65) & ( code <= 90)) {
-      System.out.println("It's a capital letter.");
+    int o = 1;
+    while (o <= 10) {
+      System.out.println(o);
+      ++o;
+    }
+    int p = 1;
 
+    do {
+      System.out.println(p);
+      ++p;
+    } while (p <= 5);
+
+    int a = 1;
+    while (a <= 10){
+      System.out.println(a);
+      ++a;
+      if (a == 4) {
+        break;
+      }
     }
-    if ((code >= 97) & ( code <= 122)) {
-      System.out.println("It's lowercase letters.");
-      
-    }
-    if (!(code < 48) && !( code > 57)) {
-      System.out.println("It's a number between 0 and 9.");
-      
-    }
-    //Using the Asky code.
   }
 }
--- study_file/day1 ~ day 7.java
+++ study_file/day1 ~ day 7.java
@@ -99,4 +99,38 @@ public class Main {
     //Using the Asky code.
   }
 }
+
+[4day]
+
+public class Main {
+  public static void main (String [] args) {
+    int sum = 0;
+
+    for (int i = 1; i<= 100; ++i){
+      sum += i;
+    }
+    System.out.println("1~100 Total = " + sum );
+
+    int o = 1;
+    while (o <= 10) {
+      System.out.println(o);
+      ++o;
+    }
+    int p = 1;
+
+    do {
+      System.out.println(p);
+      ++p;
+    } while (p <= 5);
+
+    int a = 1;
+    while (a <= 10){
+      System.out.println(a);
+      ++a;
+      if (a == 4) {
+        break;
+      }
+    }
+  }
+}
 */
