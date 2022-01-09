bddc97f7da267e9f9261c9e198839df13cc17299
kim geunho <96340564+kgh0925@users.noreply.github.com>
2022-01-09 13:12:01 +0000

    Learning about continuous and reference variables.

--- study_file/day1 ~ day 7.java
+++ study_file/day1 ~ day 7.java
@@ -133,4 +133,44 @@ public class Main {
     }
   }
 }
+
+[5day]
+public class Main {
+  public static void main (String [] args) {
+    for (int i = 1;i<=10;++i) {
+      if (i%2 != 0) {
+        continue;
+      }
+      System.out.println(i);
+    }
+
+    String str1 = "KGH";
+    String str2 = "KGH";
+    
+    if (str1 == str2) {
+      System.out.println("str1 and str2 have the same reference.");
+    }
+    else {
+      System.out.println("str1 and str2 have different references.");
+    }
+
+    if (str1.equals(str2)) {
+      System.out.println("str1 and str2 have the same string.");
+    }
+
+    String str3 = new String("KGH");
+    String str4 = new String("KGH");
+
+    if (str3 == str4) {
+      System.out.println("str3 and str4 have the same reference.");
+    }
+    else {
+      System.out.println("str3 and str4 have different references.");
+    }
+
+    if (str3.equals(str4)) {
+      System.out.println("str3 and str4 have the same string.");
+    }
+  }
+}
 */
